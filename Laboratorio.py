8faafc03ba036d4c82fbe4a6aec56ad7f194a679
yuri <yurix.pari24@gmail.com>
2026-09-01 09:58:09 -0500

    Agregar condicionales e iteraciones

--- Laboratorio.py
+++ Laboratorio.py
@@ -1,1 +1,15 @@
-print("Holamundo")
+print(" ")
+numero = int(input("Ingrese un número: "))
+
+if numero > 0:
+    print("El número es positivo")
+else:
+    print("El número es cero o negativo")
+
+contador = 1
+
+while contador <= 5:
+    print("Contador:", contador)
+    contador += 1
+
+print("Programa terminado")
